ccd3f8e300fff03962e6847bbd72ee5ba9dc9b7c
n4djib <n4djib@gmail.com>
2021-07-07 17:36:38 +0100

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a5b54b6dacf739d3847d7c89021c6090'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'bf028fe5-069f-4eec-a92e-5b9024d979fb'
+};
